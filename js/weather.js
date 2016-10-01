c4485ca736e6ae6042e45142d16009d22371b1ca
zli82016 <zhenhuali@zhenhuas-mbp.attlocal.net>
2016-09-30 23:43:09 -0400

    remove comments

--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,6 @@
 * Send the request to API to get the weather data.
 */
 function getDailyWeather(){
-	console.log("update");
 	var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22fairfax%2C%20va%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithke';
 	
 	var xhr = new XMLHttpRequest();
@@ -72,7 +71,6 @@ function updateDailyData(weatherData){
 	};
 
 	for(var i = 0; i < weatherData.length; i++){
-		console.log("weatherData: " +weatherData[i].day);
 		nodes['day' + (i + 1)].innerHTML = weatherData[i].day;
 		nodes['tempDay' + (i + 1)].innerHTML = weatherData[i].low
 			+ '&deg; / '+ weatherData[i].high + '&deg;';
